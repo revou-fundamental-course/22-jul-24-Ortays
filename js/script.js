76641b177be43ab4c629509bc472bd96d20c5627
Ortays <abdhariz071004@gmail.com>
2024-08-02 14:46:59 +0700

    Cleared

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// Ini Javascript
+function formValidation() {
+    let name = document.forms["myForm"]['fname'].value;
+    console.log(name);
+
+    if (name == '') {
+        alert('Wajib diisi!')
+    } else {
+        alert('Sukses terkirim!');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('.carousel');
+    const images = ['image1', 'image2', 'image3'];
+    let currentIndex = 0;
+
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        carousel.className = 'carousel ' + images[currentIndex];
+    }
+
+    setInterval(showNextImage, 3000);
+});
